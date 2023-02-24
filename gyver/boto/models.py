64ca0f02624841f82cb65b592d74ac3274a85ace
Gustavo Correa <self.gustavocorrea@gmail.com>
2023-02-24 11:38:59 -0300

    fix: removed boto from gyver, working on simple-aws

--- gyver/boto/models.py
+++
@@ -1,9 +0,0 @@
-from gyver.model import Model
-from gyver.utils import to_camel
-
-
-class BotoModel(Model):
-    class Config:
-        @staticmethod
-        def alias_generator(field: str):
-            return field[:1].upper() + to_camel(field[1:])
