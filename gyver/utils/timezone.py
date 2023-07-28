e6419457cd3ef828359f468fee0ce0f9f2f91aec
Gustavo Cardoso <self.gustavocorrea@gmail.com>
2023-07-28 10:35:54 -0300

    added today to utils.timezone

--- gyver/utils/timezone.py
+++ gyver/utils/timezone.py
@@ -1,6 +1,15 @@
-from datetime import datetime
+from datetime import date, datetime
+from typing import Optional
+from zoneinfo import ZoneInfo
 
 
-def now() -> datetime:
+def now(tz: Optional[ZoneInfo] = None) -> datetime:
     """Returns a datetime aware date"""
-    return datetime.now().astimezone()
+    val = datetime.now(tz)
+    if tz is None:
+        val = val.astimezone()
+    return val
+
+
+def today(tz: Optional[ZoneInfo] = None) -> date:
+    return now(tz).date()
