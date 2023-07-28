567964105f8e881dd02ac5e25157fb920513adca
Gustavo Cardoso <self.gustavocorrea@gmail.com>
2023-07-28 10:33:43 -0300

    added load and dump to utils.json

--- gyver/utils/json.py
+++ gyver/utils/json.py
@@ -7,3 +7,11 @@ loads = orjson.loads
 
 def dumps(val: typing.Any, *, default: typing.Any = None) -> str:
     return orjson.dumps(val, default=default).decode()
+
+
+def load(fdes: typing.TextIO) -> str:
+    return loads(fdes.read())
+
+
+def dump(val: typing.Any, fp: typing.TextIO, *, default: typing.Any = None):
+    return fp.write(dumps(val, default=default))
